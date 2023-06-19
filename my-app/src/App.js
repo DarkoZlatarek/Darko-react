9edbfe272f025317de3270e7a4ed1449e0547632
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-06-19 21:15:52 +0000

    Add StetfulGreeting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32"/>
+      <StatefulGreetingWithPrevState greeting="I am a stateful class component!" name="Michael"/>
     </div>
   );
 }
