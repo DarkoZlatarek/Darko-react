b3367b8197808ba97f986f3ceb630dc264174302
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-06-21 19:41:35 +0000

    Add RenderingLists

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-import StatefulGreeting from './components/StatefulGreeting';
-import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
-import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
-import EventsFunctional from './components/EventsFunctional';
-import EventsClass from './components/EventsClass';
-import EventBinding from './components/EventBinding';
-import ConditionalRenderingClass from './components/ConditionalRenderingClass';
-import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
-import NestingComponents from './components/NestingComponents';
-import MethodAsPropsParent from './components/MethodAsPropsParent';
+import RenderingLists from './components/RenderingLists';
 
 
 function App() {
   return (
     <div className="App">
-      <MethodAsPropsParent />
+      <RenderingLists />
     </div>
   );
 }
