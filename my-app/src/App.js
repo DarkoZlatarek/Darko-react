4aa0a1575abeb57ed0b93d7818df7e4ab14b9702
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-06-26 21:07:52 +0000

    Use of hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import SearchBar from './components/SearchBar';
+import UseEffectCounter from './components/UseEffectCounter';
 
 
 function App() {
   return (
     <div className="App">
-      <SearchBar />
+      <UseEffectCounter />
     </div>
   );
 }
