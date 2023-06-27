b15bc24783e3165a952bae15872dae82f495fb24
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-06-27 17:51:27 +0000

    conditional hooks

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -1,18 +1,34 @@
 import React, {useState, useEffect} from 'react'
 
 function UseEffectCounter() {
-    const [count, setCount] = useState(0);
-    useEffect(() => {
-        document.title = count;
-    });
+  const [count, setCount] = useState(0);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    console.log('Count 1 effect')
+    document.title = count;
+  }, [count]);
+
+  useEffect(() => {
+    console.log('Creating timer')
+    const interval = setInterval(() => {
+      console.log('Interval executed');
+      setTime(time => time + 1)
+    }, 1000);
+    return () => {
+      console.log('Cleaning up')
+      clearInterval(interval);
+    }
+  }, []);
 
   return (
     <div>
-        <button onClick={
-            () => setCount(count => count + 1)
-            }>
-            Increment count ({count})
-        </button>
+      <button onClick={
+        () => setCount(count => count + 1)
+      }>
+        Increment count ({count})
+      </button>
+      <h2>Time is {time}</h2>
     </div>
   )
 }
